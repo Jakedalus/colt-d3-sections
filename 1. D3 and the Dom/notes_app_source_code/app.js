6e27bf557100d014cef63094d944239ed93d97c5
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-04 18:05:38 -0500

    Add preview feature to notes app

--- 1. D3 and the Dom/notes_app_source_code/app.js
+++ 1. D3 and the Dom/notes_app_source_code/app.js
@@ -1,14 +1,54 @@
 // preview feature
 
-d3.select('#new-note').on('submit', function() {
+function addNote(fontColor) {
+	if (fontColor) {
+		d3.event.preventDefault();
+		var input = d3.select('input');
+		d3
+			.select('#notes')
+			.append('p')
+			.classed('note', true)
+			.style('color', fontColor)
+			.text(input.property('value'));
+	} else {
+		d3.event.preventDefault();
+		var input = d3.select('input');
+		d3
+			.select('#notes')
+			.append('p')
+			.classed('note', true)
+			.text(input.property('value'));
+	}
+	input.property('value', '');
+	d3.select('#preview').style('display', 'none');
+}
+
+d3.select('input').on('input', function() {
 	d3.event.preventDefault();
 	var input = d3.select('input');
 	d3
-		.select('#notes')
-		.append('p')
+		.select('#preview')
 		.classed('note', true)
+		.style('display', 'block')
 		.text(input.property('value'));
-	input.property('value', '');
+
+	// console.log('d3.event', d3.event);
+	if (input.property('value') === '') {
+		d3.select('#preview').style('display', 'none');
+	}
+});
+
+d3.select('#new-note').on('submit', function() {
+	// d3.event.preventDefault();
+	// var input = d3.select('input');
+	// d3
+	// 	.select('#notes')
+	// 	.append('p')
+	// 	.classed('note', true)
+	// 	.text(input.property('value'));
+	// input.property('value', '');
+
+	addNote();
 });
 
 d3.select('#remove-all-notes').on('click', function() {
@@ -21,17 +61,9 @@ function getRandomInt(max) {
 }
 
 d3.select('#im-feeling-lucky').on('click', function() {
-	d3.event.preventDefault();
-	var input = d3.select('input');
 	const fontColor = `rgb(${getRandomInt(
 		255
 	)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
 	console.log('fontColor', fontColor);
-	d3
-		.select('#notes')
-		.append('p')
-		.classed('note', true)
-		.style('color', fontColor)
-		.text(input.property('value'));
-	input.property('value', '');
+	addNote(fontColor);
 });
