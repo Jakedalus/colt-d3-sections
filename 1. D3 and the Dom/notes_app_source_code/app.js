2a87f64cce34474846486efb3b99beb19753ffd6
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-04 17:41:13 -0500

    Add remove-all-note button and make it work

--- 1. D3 and the Dom/notes_app_source_code/app.js
+++ 1. D3 and the Dom/notes_app_source_code/app.js
@@ -1,10 +1,19 @@
-d3.select("#new-note")
-    .on('submit', function() {
-      d3.event.preventDefault();
-      var input = d3.select('input');
-      d3.select("#notes")
-        .append('p')
-          .classed('note', true)
-          .text(input.property('value'));
-      input.property('value', '');
-    });
+// remove all notes button
+// i'm feeling lucky, random styling
+// preview feature
+
+d3.select('#new-note').on('submit', function() {
+	d3.event.preventDefault();
+	var input = d3.select('input');
+	d3
+		.select('#notes')
+		.append('p')
+		.classed('note', true)
+		.text(input.property('value'));
+	input.property('value', '');
+});
+
+d3.select('#remove-all-notes').on('click', function() {
+	d3.event.preventDefault();
+	d3.select('#notes').selectAll('p').remove();
+});
