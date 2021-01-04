fd8b607e8d5cd68ccf11ce5cab746ec1f5e6cfb5
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-04 17:46:14 -0500

    Add im-feeling-lucky button and randomly style the text color of a new note

--- 1. D3 and the Dom/notes_app_source_code/app.js
+++ 1. D3 and the Dom/notes_app_source_code/app.js
@@ -1,5 +1,3 @@
-// remove all notes button
-// i'm feeling lucky, random styling
 // preview feature
 
 d3.select('#new-note').on('submit', function() {
@@ -16,4 +14,24 @@ d3.select('#new-note').on('submit', function() {
 d3.select('#remove-all-notes').on('click', function() {
 	d3.event.preventDefault();
 	d3.select('#notes').selectAll('p').remove();
+});
+
+function getRandomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
+
+d3.select('#im-feeling-lucky').on('click', function() {
+	d3.event.preventDefault();
+	var input = d3.select('input');
+	const fontColor = `rgb(${getRandomInt(
+		255
+	)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
+	console.log('fontColor', fontColor);
+	d3
+		.select('#notes')
+		.append('p')
+		.classed('note', true)
+		.style('color', fontColor)
+		.text(input.property('value'));
+	input.property('value', '');
 });
