84984893b5b44c7e3ad085832af7cf6811faed4d
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-25 16:48:44 -0500

    Add first part of pie chart example

---
+++ 3. D3 and SVG/d3-pie-chart/app.js
@@ -0,0 +1,46 @@
+var minYear = d3.min(birthData, d => d.year);
+var width = 600;
+var height = 600;
+var yearData = birthData.filter(d => d.year === minYear);
+
+var continents = [];
+for (let d of birthData) {
+	var continent = d.continent;
+	if (continents.indexOf(continent) === -1) {
+		continents.push(continent);
+	}
+}
+
+const colorScale = d3
+	.scaleOrdinal()
+	.domain(continents)
+	.range(d3.schemeCategory10);
+
+d3
+	.select('svg')
+	.attr('width', width)
+	.attr('height', height)
+	.append('g')
+	.attr(
+		'transform',
+		`translate(${width / 2}, ${height / 2})`
+	)
+	.classed('chart', true);
+
+const arcs = d3.pie().value(d => d.births)(yearData);
+
+const path = d3
+	.arc()
+	.outerRadius(width / 2 - 10)
+	.innerRadius(width / 4);
+
+d3
+	.select('.chart')
+	.selectAll('.arc')
+	.data(arcs)
+	.enter()
+	.append('path')
+	.classed('arc', true)
+	.attr('fill', d => colorScale(d.data.continent))
+	.attr('stroke', 'black')
+	.attr('d', path);
