4ca87a7714ef274d8c20a5535037ef5a9eeb83a7
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-01-27 18:00:53 -0500

    Add basic d3.json and d3.csv calls

--- 5. D3 Odds and Ends/managing_async/app.js
+++ 5. D3 Odds and Ends/managing_async/app.js
@@ -1,1 +1,31 @@
-// write your code here!
+d3.json('./countries.json', (error, data) => {
+	if (error) throw error;
+
+	// console.log('data', data);
+
+	d3
+		.select('body')
+		.selectAll('h3')
+		.data(data.geonames)
+		.enter()
+		.append('h3')
+		.text(d => d.countryName);
+});
+
+d3.csv(
+	'./simplemaps-worldcities-basic.csv',
+	(row, i, headers) => {
+		if (+row.pop < 10000) return;
+
+		return {
+			cityName    : row.city,
+			countryCode : row.iso2,
+			population  : +row.pop
+		};
+	},
+	(error, data) => {
+		if (error) throw error;
+
+		console.log('data', data);
+	}
+);
