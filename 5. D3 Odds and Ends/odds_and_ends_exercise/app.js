f9f4c34e957fd7801a5ff1f0725cde663281b123
Jacob A. Carpenter <jacquedalus@gmail.com>
2021-02-01 15:55:10 -0500

    Get bar chart for current day deaths by state to display after loading json from COVID Tracking Project

---
+++ 5. D3 Odds and Ends/odds_and_ends_exercise/app.js
@@ -0,0 +1,53 @@
+// fetch('https://api.covidtracking.com/v1/us/daily.json')
+// 	.then(response => {
+// 		console.log('response', response);
+// 		return response.json();
+// 	})
+// 	.then(data => {
+// 		console.log('data', data);
+// 	});
+
+d3
+	.queue()
+	.defer(d3.json, './dailyCOVID_states.json')
+	.await((error, data) => {
+		console.log('data', data);
+
+		const minDate = d3.min(data, d => d.date);
+		const maxDate = d3.max(data, d => d.date);
+
+		console.log('minDate, maxDate', minDate, maxDate);
+
+		const dayData = data.filter(d => d.date === maxDate);
+
+		console.log('dayData', dayData);
+
+		const width = 800,
+			height = 600,
+			barPadding = 10;
+
+		const numBars = dayData.length;
+
+		const barWidth = width / numBars - barPadding;
+
+		const maxDeaths = d3.max(dayData, d => d.death);
+
+		const yScale = d3
+			.scaleLinear()
+			.domain([ 0, maxDeaths ])
+			.range([ height, 0 ]);
+
+		const chart = d3
+			.select('svg')
+			.attr('width', width)
+			.attr('height', height)
+			.selectAll('.bar')
+			.data(dayData)
+			.enter()
+			.append('rect')
+			.classed('bar', true)
+			.attr('width', barWidth)
+			.attr('height', d => height - yScale(d.death))
+			.attr('x', (d, i) => (barWidth + barPadding) * i)
+			.attr('y', d => yScale(d.death));
+	});
